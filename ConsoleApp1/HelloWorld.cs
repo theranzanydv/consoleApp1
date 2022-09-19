58b75ba97ae2cdb38d33bb7cc0726f17b4da1de0
Ranjan Yadav <theranzanydv@gmail.com>
2022-09-19 09:29:46 +0545

    initial commit

---
+++ ConsoleApp1/HelloWorld.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp1
+{
+    //
+    // public class HelloWorld
+    // {
+    //     public HelloWorld()
+    //     {
+    //         Console.WriteLine('f');
+    //     }
+    //
+    //     public void Hello()
+    //     {
+    //         Console.WriteLine('H');
+    //         
+    //     }
+    internal class Hello
+    {
+        public void HelloWorld()
+        {
+            Console.WriteLine("Hello Bitces");
+
+        }
+
+    }
+}
+// }
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using ConsoleApp1;
+
+Console.WriteLine("Welcome to .NET with C#");
+string? input = Console.ReadLine();
+
+Console.WriteLine("The number of times you want to repeat the name");
+string? times = Console.ReadLine();
+Console.WriteLine("\n~~~~~~~~~~~~~~~~~~~~");
+
+int intTimes = Convert.ToInt32(times);
+// intTimes = intTimes * 10;
+
+for (int i = 0; i < intTimes; i++)
+{
+    Console.WriteLine("Hello " + input);
+}
+
+Student student = new Student();
+student.Name = "Ranjan";
+student.Age = 23;
+student.Id = 1;
+
+Console.WriteLine(student.Id);
+Console.WriteLine(student.Name);
+Console.WriteLine(student.Age);
+
+Console.ReadKey();
